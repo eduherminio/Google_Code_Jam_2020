8c785aba462371b93990b1d5d597d0411ab01637
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2020-04-05 15:53:06 +0200

    Spaces

--- NestingDepth/NestingDepth.cpp
+++ NestingDepth/NestingDepth.cpp
@@ -10,69 +10,69 @@ void HandleTestCase(int);
 
 int main()
 {
-	int testCases; cin >> testCases;
+    int testCases; cin >> testCases;
 
-	for (int i = 0; i < testCases; ++i)
-	{
-		HandleTestCase(i + 1);
-	}
+    for (int i = 0; i < testCases; ++i)
+    {
+        HandleTestCase(i + 1);
+    }
 }
 
 void HandleTestCase(int caseNumber)
 {
-	std::string input; cin >> input;
-	std::vector<int> numbers;
-	for (int i = 0; i < input.size(); ++i)
-	{
-		numbers.push_back(input[i] - '0');
-	}
+    std::string input; cin >> input;
+    std::vector<int> numbers;
+    for (int i = 0; i < input.size(); ++i)
+    {
+        numbers.push_back(input[i] - '0');
+    }
 
-	const char closingBracket = ')';
-	const char openingBracket = '(';
-	std::string output;
-	auto arraySize = numbers.size();
+    const char closingBracket = ')';
+    const char openingBracket = '(';
+    std::string output;
+    auto arraySize = numbers.size();
 
-	std::reverse(std::begin(numbers), std::end(numbers));
+    std::reverse(std::begin(numbers), std::end(numbers));
 
-	int closedNumber = 0;
-	for (int i = 0; i < arraySize; ++i)
-	{
-		int n = numbers[i];
-		int n_prev = i == 0
-			? 0
-			: numbers[i - 1];
-		int n_next = i == arraySize - 1 ? 0 : numbers[i + 1];
+    int closedNumber = 0;
+    for (int i = 0; i < arraySize; ++i)
+    {
+        int n = numbers[i];
+        int n_prev = i == 0
+            ? 0
+            : numbers[i - 1];
+        int n_next = i == arraySize - 1 ? 0 : numbers[i + 1];
 
-		if (n_prev == 0 || n_prev < n)
-		{
-			while (n != closedNumber)
-			{
-				++closedNumber;
-				output += closingBracket;
-			}
-		}
+        if (n_prev == 0 || n_prev < n)
+        {
+            while (n != closedNumber)
+            {
+                ++closedNumber;
+                output += closingBracket;
+            }
+        }
 
-		output += std::to_string(n);
+        output += std::to_string(n);
 
-		if (n_next == 0)
-		{
-			while (closedNumber)
-			{
-				output += openingBracket;
-				--closedNumber;
-			}
-		}
-		else if (n > n_next)
-		{
-			for (int p = 0; p < n - n_next; ++p)
-			{
-				output += openingBracket;
-				--closedNumber;
-			}
-		}
-	}
+        if (n_next == 0)
+        {
+            while (closedNumber)
+            {
+                output += openingBracket;
+                --closedNumber;
+            }
+        }
+        else if (n > n_next)
+        {
+            for (int p = 0; p < n - n_next; ++p)
+            {
+                output += openingBracket;
+                --closedNumber;
+            }
+        }
+    }
 
-	std::reverse(std::begin(output), std::end(output));
+    std::reverse(std::begin(output), std::end(output));
 
-	cout << "Case #" << caseNumber << ": " << output << std::endl;
+    cout << "Case #" << caseNumber << ": " << output << std::endl;
 }
--- ParentingPartnering/ParentingPartnering.cpp
+++ ParentingPartnering/ParentingPartnering.cpp
@@ -12,112 +12,112 @@ void HandleTestCase(int);
 
 int main()
 {
-	int testCases; cin >> testCases;
+    int testCases; cin >> testCases;
 
-	for (int i = 0; i < testCases; ++i)
-	{
-		HandleTestCase(i + 1);
-	}
+    for (int i = 0; i < testCases; ++i)
+    {
+        HandleTestCase(i + 1);
+    }
 }
 
 class Activity
 {
 public:
-	int Number;
-	int Start;
-	int End;
-	std::string Person;
+    int Number;
+    int Start;
+    int End;
+    std::string Person;
 
-	Activity(int number, int start, int end)
-		:Number(number), Start(start), End(end)
-	{
-	}
+    Activity(int number, int start, int end)
+        :Number(number), Start(start), End(end)
+    {
+    }
 };
 
 void HandleTestCase(int caseNumber)
 {
-	std::string output;
-	int numberOfActivities; cin >> numberOfActivities;
-	std::vector<Activity> activities; activities.reserve(numberOfActivities);
+    std::string output;
+    int numberOfActivities; cin >> numberOfActivities;
+    std::vector<Activity> activities; activities.reserve(numberOfActivities);
 
-	for (int i = 0; i < numberOfActivities; ++i)
-	{
-		int start; cin >> start;
-		int end; cin >> end;
-		activities.push_back(Activity(i, start, end));
-	}
+    for (int i = 0; i < numberOfActivities; ++i)
+    {
+        int start; cin >> start;
+        int end; cin >> end;
+        activities.push_back(Activity(i, start, end));
+    }
 
 
-	std::sort(std::begin(activities), std::end(activities), [](const auto& a, const auto& b)
-	{
-		return a.Start < b.Start;
-	});
+    std::sort(std::begin(activities), std::end(activities), [](const auto& a, const auto& b)
+    {
+        return a.Start < b.Start;
+    });
 
-	bool cameronFree = true;
-	bool jamieFree = true;
+    bool cameronFree = true;
+    bool jamieFree = true;
 
-	auto maxT = std::max_element(std::begin(activities), std::end(activities), [](const auto& a, const auto& b) { return a.End < b.End; })->End;
+    auto maxT = std::max_element(std::begin(activities), std::end(activities), [](const auto& a, const auto& b) { return a.End < b.End; })->End;
 
-	for (int t = 0; t < maxT; ++t)
-	{
-		auto result = std::begin(activities);
-		while (result != std::end(activities))
-		{
-			result = std::find_if(result, std::end(activities), [t](const auto& act) { return act.End == t; });
-			if (result != std::end(activities))
-			{
-				if (result->Person == "C")
-				{
-					cameronFree = true;
-				}
-				else if (result->Person == "J")
-				{
-					jamieFree = true;
-				}
+    for (int t = 0; t < maxT; ++t)
+    {
+        auto result = std::begin(activities);
+        while (result != std::end(activities))
+        {
+            result = std::find_if(result, std::end(activities), [t](const auto& act) { return act.End == t; });
+            if (result != std::end(activities))
+            {
+                if (result->Person == "C")
+                {
+                    cameronFree = true;
+                }
+                else if (result->Person == "J")
+                {
+                    jamieFree = true;
+                }
 
-				++result;
-			}
-		}
+                ++result;
+            }
+        }
 
-		result = std::begin(activities);
-		while (result != std::end(activities))
-		{
-			result = std::find_if(result, std::end(activities), [t](const auto& act) { return act.Start == t; });
-			if (result != std::end(activities))
-			{
-				if (cameronFree)
-				{
-					cameronFree = false;
-					result->Person = "C";
-				}
-				else if (jamieFree)
-				{
-					jamieFree = false;
-					result->Person = "J";
-				}
-				else
-				{
-					output = "IMPOSSIBLE";
-					break;
-				}
+        result = std::begin(activities);
+        while (result != std::end(activities))
+        {
+            result = std::find_if(result, std::end(activities), [t](const auto& act) { return act.Start == t; });
+            if (result != std::end(activities))
+            {
+                if (cameronFree)
+                {
+                    cameronFree = false;
+                    result->Person = "C";
+                }
+                else if (jamieFree)
+                {
+                    jamieFree = false;
+                    result->Person = "J";
+                }
+                else
+                {
+                    output = "IMPOSSIBLE";
+                    break;
+                }
 
-				result++;
-			}
-		}
-	}
+                result++;
+            }
+        }
+    }
 
-	if (output != "IMPOSSIBLE")
-	{
-		std::sort(std::begin(activities), std::end(activities), [](const auto& a, const auto& b)
-		{
-			return a.Number < b.Number;
-		});
+    if (output != "IMPOSSIBLE")
+    {
+        std::sort(std::begin(activities), std::end(activities), [](const auto& a, const auto& b)
+        {
+            return a.Number < b.Number;
+        });
 
-		for (const auto& activity : activities)
-		{
-			output += activity.Person;
-		}
-	}
+        for (const auto& activity : activities)
+        {
+            output += activity.Person;
+        }
+    }
 
-	cout << "Case #" << caseNumber << ": " << output << std::endl;
+    cout << "Case #" << caseNumber << ": " << output << std::endl;
 }
